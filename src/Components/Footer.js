8d0c9c4128dfed3dbe97eae9b60c98de85c41a7a
Abhinav Tyagi <tyagi_a@India.XoriantCorp.com>
2021-02-16 20:46:02 +0530

    second commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-bottom">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-bottom">
         <a className="navbar-brand" href="https://www.linkedin.com/abhinav-08">This Web app has been developed by Abhinav Tyagi</a>
     </nav>
     )
